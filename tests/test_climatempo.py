b52fada638dada2e5cd8e978d327b4c1008b42d8
Felipe <felipeiasi@gmail.com>
2019-04-09 18:35:18 -0300

    Código de teste

---
+++ tests/test_climatempo.py
@@ -0,0 +1,12 @@
+from climatempo_api.climatempo import Climatempo
+from climatempo_api.climatempo import BASE_URL, VERSION
+
+def test_criar_climatempo():
+    climatempo = Climatempo()
+    assert climatempo is not None
+
+def test_formatar_endpoint():
+    climatempo= Climatempo("***")
+    assert  climatempo._formatar_endpoint("/test/") == f'{BASE_URL}{VERSION}/test/?token=***'
+
+
