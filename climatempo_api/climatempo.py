a1e51c93532db59b31d6044668e36baa8826bcda
Felipe Costa <felipeiasi@gmail.com>
2019-04-09 21:41:55 -0300

    Criacao das respectivas chamadas

--- climatempo_api/climatempo.py
+++ climatempo_api/climatempo.py
@@ -11,7 +11,7 @@ class Climatempo():
     # === Climate ---
     def chuva_climatica(self, id: int):
         """
-        Chuva climatica por ID
+        Retorna a chuva climatica da cidade fornecida por ID
 
         :param id: ID da cidade
         :return: Dicionario com os dados
@@ -20,22 +20,61 @@ class Climatempo():
         return self._requisitar_dados(endpoint)
 
     # --- Forecast ---
-    def previsao_15_dias(self):
-        return NotImplemented
+    def previsao_15_dias(self, id: int):
+        """
+        Retorna a previão para 15 dias da cidade fornecida por ID.
 
-    def previsao_72_horas(self):
-        return NotImplemented
+        :param id: ID da cidade
+        :return: Dicionario com os dados
+        """
+        endpoint = self._formatar_endpoint(f"/forecast/locale/{id}/days/15")
+        return self._requisitar_dados(endpoint)
+
+    def previsao_72_horas(self, id: int):
+        """
+        Retorna a previsão para 72 horas da cidade fornecida por ID.
+
+        :param id: ID da cidade
+        :return: Dicionario com os dados
+        """
+        endpoint = self._formatar_endpoint(f"/forecast/locale/{id}/hours/72")
+        return self._requisitar_dados(endpoint)
 
     # --- Locale ---
     def busca_cidade_ID(self, id: int):
-        return NotImplemented
+        """
+        Retorna os dados da cidade para o ID especificado.
 
-    def busca_cidade_nome(self, nome: str = "", estado: str = ""):
-        return NotImplemented
+        :param id: ID da cidade
+        :return: Dicionario com os dados
+        """
+
+        endpoint = self._formatar_endpoint(f"/locale/city/{id}")
+        return self._requisitar_dados(endpoint)
+
+    def busca_cidade_nome(self, cidade: str = "", estado: str = ""):
+        """
+        Retorna os dados de uma cidade para a cidade e o estado fornecido.
+
+        :param cidade:
+        :param estado:
+        :return: Dicionario com os dados
+        """
+
+        endpoint = self._formatar_endpoint(f"/locale/city?name={cidade}&state={estado}")
+        return self._requisitar_dados(endpoint)
 
     # --- Weather ---
     def tempo_momento(self, id: int):
-        return NotImplemented
+        """
+        Retorna o tempo no momento para o ID especificado.
+
+        :param id: ID da cidade
+        :return: Dicionario com os dados
+        """
+
+        endpoint = self._formatar_endpoint(f"/weather/locale/{id}/current")
+        return self._requisitar_dados(endpoint)
 
     def _formatar_endpoint(self, endpoint: str) -> str:
         return f'{BASE_URL}{VERSION}{endpoint}?token={self.token}'
